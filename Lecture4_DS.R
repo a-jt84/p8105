b3d5a9cca3f203f72d06d5015a73d0a4c792cbab
Andy Turner <andy.jturner2497@gmail.com>
2023-09-14 10:40:55 -0400

    Adding a R file with some documentation

---
+++ Lecture4_DS.R
@@ -0,0 +1,12 @@
+# Slight differences with the Git R Project - there isa Git client on the right hand side
+# Also a README file and a git ignore file too 
+
+#When I click the two files in Git they switch to Green As for Add. Once I commit then it
+# is officially added. 
+
+# Now I can do some work in the Git repositroy. 
+
+usethis::create_github_token()
+# Getting our GitHub Token
+
+gitcreds::gitcreds_set()
